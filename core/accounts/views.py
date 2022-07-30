43e8631cb9e99a9213da4408f996a5e5b2524307
Iman-Daneshi <iman_daneshi@yahoo.com>
2022-07-30 13:12:08 +0430

    background process with celery

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,11 @@
 # from django.shortcuts import render
+from django.http import HttpResponse
+# import time
+from .tasks import sendemail
 
 # Create your views here.
+
+
+def send_email(request):
+    sendemail.delay()
+    return HttpResponse("<h1>Done sending</h1>")
